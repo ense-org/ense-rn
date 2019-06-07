5b078a7a5e0664742900d89aa31bd9707234fabd
rob cheung <kousun12@gmail.com>
2019-06-07 16:18:09 -0400

    --wip-- [skip ci]

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,16 @@
 // @flow
 import { get } from 'lodash';
+import axios from 'axios';
 import { store } from 'redux/store';
 
 const localDev = true;
 export const API_BASE = localDev ? 'http://en.se:3000' : 'https://api.ense.nyc';
 // await fetch(url, {})
 export const CLIENT_ID = 'PfE36O4PtvqmtPZf9VCXaf3D00GBGVGwn8VsPVqBLUy88POt';
+
+const instance = axios.create({
+  baseURL: API_BASE,
+});
 
 export const urlFor = (path: string): string => `${API_BASE}${path}`;
 
@@ -45,20 +50,41 @@ export const $post = (
   const options = {
     ...extraOptions,
     method: 'POST',
-    // headers: {
-    // Accept: 'application/json',
-    // 'Content-Type': 'multipart/form-data',
-    // 'Content-Type': 'application/json',
-    // ...extraHeaders,
-    // ...(deviceSecretKey && { Authorization }),
-    // },
+    headers: {
+      // 'Content-Type': 'multipart/form-data',
+      // 'Content-Type': 'application/json',
+      ...extraHeaders,
+      ...(deviceSecretKey && { Authorization }),
+    },
     ...body,
   };
   const url = urlFor(path);
-  console.log(url, options);
   return fetch(url, options)
     .then(_deserialize)
     .catch(console.error);
+};
+
+export const $postAxios = (
+  path: string,
+  params: ?Object,
+  extraOptions?: Object,
+  extraHeaders?: Object
+): Promise<any> => {
+  const options = {
+    data: params,
+  };
+  console.log(path, options);
+
+  return axios({
+    method: 'post',
+    baseURL: API_BASE,
+    url: path,
+    data: params,
+  }).catch(console.error);
+  // return instance
+  //   .post(path, params)
+  //   .then(_deserialize)
+  //   .catch(console.error);
 };
 
 const queryString = (params: Object): string => {
@@ -77,3 +103,13 @@ const formData = (params: Object): FormData => {
 
 const _deserialize = (r: Response): any =>
   r.headers.get('content-type').includes('json') ? r.json() : r.text();
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}

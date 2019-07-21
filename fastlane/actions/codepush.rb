7275b8b13e79fa918def1f62014a71faf85b7ac0
rob cheung <kousun12@gmail.com>
2019-07-20 23:13:47 -0400

    --wip-- [skip ci]

--- fastlane/actions/codepush.rb
+++ fastlane/actions/codepush.rb
@@ -1,13 +1,8 @@
 module Fastlane
   module Actions
-    module SharedValues
-      CODEPUSH_CUSTOM_VALUE = :CODEPUSH_CUSTOM_VALUE
-    end
-
     class CodepushAction < Action
       def self.run(params)
-        # fastlane will take care of reading in the parameter and fetching the environment variable:
-        cmd = "appcenter codepush release-react #{params[:opt_string]}"
+        cmd = "npx appcenter codepush release-react #{params[:opt_string]}"
         UI.message cmd
         sh cmd
       end
@@ -21,15 +16,10 @@ module Fastlane
       end
 
       def self.details
-        # Optional:
-        # this is your chance to provide a more detailed description of this action
         "build and upload a new js bundle to appcenter"
       end
 
       def self.available_options
-        # Define all options your action supports.
-
-        # Below a few examples
         [
           FastlaneCore::ConfigItem.new(key: :opt_string,
             description: "codepush release-react opt string",
@@ -41,13 +31,10 @@ module Fastlane
       end
 
       def self.output
-        # Define the shared values you are going to provide
-        # Example
         []
       end
 
       def self.return_value
-        # If your method provides a return value, you can describe here what it does
       end
 
       def self.authors
@@ -55,16 +42,7 @@ module Fastlane
       end
 
       def self.is_supported?(platform)
-        # you can do things like
-        #
-        #  true
-        #
-        #  platform == :ios
-        #
-        #  [:ios, :mac].include?(platform)
-        #
-
-        platform == :ios || platform == :android
+        [:ios, :android].include?(platform)
       end
     end
   end

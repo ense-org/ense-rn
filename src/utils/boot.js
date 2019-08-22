b8308e514e51bff4b49a7a1e8966481bc255e6f8
rob cheung <kousun12@gmail.com>
2019-08-22 17:07:06 -0400

    --wip-- [skip ci]

--- src/utils/boot.js
+++ src/utils/boot.js
@@ -3,6 +3,14 @@ import 'intl/locale-data/jsonp/en';
 import 'redux/store'; // store sets a global var `store`
 // eslint-disable-next-line import/no-unresolved
 import { useScreens } from 'react-native-screens';
+import { YellowBox } from 'react-native';
 import '@js-joda/timezone';
 
 useScreens();
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillReceiveProps is deprecated',
+  'Warning: componentWillUpdate is deprecated',
+  'Warning: componentWillMount is deprecated',
+  'Remote debugger is in a background',
+]);

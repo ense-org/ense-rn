e7042b214584922027e7f398932d15af8dc90def
rob cheung <kousun12@gmail.com>
2019-11-11 19:53:00 -0500

    --wip-- [skip ci]

--- src/utils/boot.js
+++ src/utils/boot.js
@@ -4,9 +4,19 @@ import 'redux/store'; // store sets a global var `store`
 // eslint-disable-next-line import/no-unresolved
 import { useScreens } from 'react-native-screens';
 import { YellowBox } from 'react-native';
+import git from './git';
 import '@js-joda/timezone';
 
 useScreens();
+
+import {
+  Client as BSClient,
+  Configuration as BSCfg,
+} from 'bugsnag-react-native';
+
+const codeVersion = `${git.sha}.${Platform.OS}`;
+const cfg = new BSCfg('6d50669a24c902d712d35af166dee4e0');
+cfg.codeBundleId = codeVersion;
 
 YellowBox.ignoreWarnings([
   'Warning: componentWillReceiveProps is deprecated',
---
+++ src/utils/git.js
@@ -0,0 +1,1 @@
+export default { sha: '1b25e5b0a884fb8b45ccf09ce2df9db8d4cc5e9d' };
